298b4ffd21f29b670c84594ef843b493292b831a
lewypopescu <popesculewy@gmail.com>
2024-04-13 18:48:37 +0300

    first done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryStart = document.querySelector(".gallery");
+
+function imagesCreate() {
+  for (const image of galleryItems) {
+    const imageMade = document.createElement("li");
+    imageMade.classList.add("gallery__item");
+    imageMade.innerHTML = `<a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>`;
+    galleryStart.append(imageMade);
+  }
+  const imageClick = galleryStart.querySelectorAll(".gallery__image");
+  imageClick.forEach((image) => {
+    image.addEventListener("click", (event) => {
+      event.preventDefault();
+      const instance = basicLightbox.create(
+        `<img src="${image.dataset.source}" alt="${image.alt}">`
+      );
+      instance.show();
+      document.addEventListener("keyup", (event) => {
+        if (event.code === "Escape") {
+          instance.close();
+        }
+      });
+    });
+  });
+}
+
+imagesCreate();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,1 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-console.log(galleryItems);
+import { galleryItems } from "./gallery-items.js";
